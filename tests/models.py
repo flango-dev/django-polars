021585a8efc0395ac238a284afe78e4f88bdf903
Florian Kromer <kromer-florian@protonmail.com>
2024-06-27 19:55:37 +0200

    add docs/tests

--- tests/models.py
+++ tests/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 
 
+class PkBuiltinFieldModel(models.Model):
+    int: models.IntegerField = models.IntegerField(primary_key=True)
+    bigint: models.BigIntegerField = models.BigIntegerField()
+    float: models.FloatField = models.FloatField()
+    decimal: models.DecimalField = models.DecimalField(max_digits=3, decimal_places=2)
+    text: models.TextField = models.TextField()
+
+
 class BuiltinFieldModel(models.Model):
+    # https://docs.djangoproject.com/en/5.0/topics/db/models/#automatic-primary-key-fields
     int: models.IntegerField = models.IntegerField()
     bigint: models.BigIntegerField = models.BigIntegerField()
     float: models.FloatField = models.FloatField()
     decimal: models.DecimalField = models.DecimalField(max_digits=3, decimal_places=2)
     text: models.TextField = models.TextField()
+
+
+class View(models.Model):
+    bigint: models.BigIntegerField = models.BigIntegerField()
+    float: models.FloatField = models.FloatField()
+    decimal: models.DecimalField = models.DecimalField(max_digits=3, decimal_places=2)
+
+    class Meta:
+        managed = False
+        db_table = "view"
--- tests/test_io.py
+++ tests/test_io.py
@@ -4,11 +4,11 @@ from django.db import connection
 from polars.testing import assert_frame_equal
 
 from django_polars.io import read_frame
-from tests.models import BuiltinFieldModel
+from tests.models import BuiltinFieldModel, PkBuiltinFieldModel, View
 
 
-@pytest.mark.django_db
-def test_df_builtin_field_type_model_df_roundtrip():
+@pytest.fixture
+def builtin_field_model_dataframe() -> pl.DataFrame:
     data = {
         "int": [1, 2],
         "bigint": [100000, 200000],
@@ -16,8 +16,44 @@ def test_df_builtin_field_type_model_df_roundtrip():
         "decimal": [0.01, 0.02],
         "text": ["bla", "blub"],
     }
-    df_in = pl.DataFrame(data)
-    for cols in df_in.rows(named=True):
+    return pl.DataFrame(data)
+
+
+@pytest.fixture
+def builtin_field_model_fixture() -> None:
+    BuiltinFieldModel.objects.create(
+        int=1, bigint=100000, float=0.1, decimal=0.01, text="bla"
+    )
+    BuiltinFieldModel.objects.create(
+        int=2, bigint=200000, float=0.2, decimal=0.02, text="blub"
+    )
+    return None
+
+
+@pytest.fixture
+def builtin_view_dataframe() -> pl.DataFrame:
+    data = {
+        "bigint": [100000, 200000],
+        "float": [0.1, 0.2],
+        "decimal": [0.01, 0.02],
+    }
+    return pl.DataFrame(data)
+
+
+@pytest.fixture
+def pk_builtin_field_model_fixture() -> None:
+    PkBuiltinFieldModel.objects.create(
+        int=1, bigint=100000, float=0.1, decimal=0.01, text="bla"
+    )
+    PkBuiltinFieldModel.objects.create(
+        int=2, bigint=200000, float=0.2, decimal=0.02, text="blub"
+    )
+    return None
+
+
+@pytest.mark.django_db
+def test_df_builtin_field_type_model_df_roundtrip(builtin_field_model_dataframe):
+    for cols in builtin_field_model_dataframe.rows(named=True):
         BuiltinFieldModel.objects.create(
             int=cols["int"],
             bigint=cols["bigint"],
@@ -27,11 +63,33 @@ def test_df_builtin_field_type_model_df_roundtrip():
         )
 
     df_out = read_frame(con=connection, qs=BuiltinFieldModel.objects.filter(int=1))
-    assert_frame_equal(df_in.slice(0, 1), df_out)
+    assert_frame_equal(builtin_field_model_dataframe.slice(0, 1), df_out)
 
     s = pl.Series("id", [1, 2])
-    df_in.insert_column(0, s)
+    builtin_field_model_dataframe.insert_column(0, s)
     df_out_1 = read_frame(
         con=connection, qs=BuiltinFieldModel.objects.filter(int=1), drop_idx=False
     )
-    assert_frame_equal(df_in.slice(0, 1), df_out_1)
+    assert_frame_equal(builtin_field_model_dataframe.slice(0, 1), df_out_1)
+
+
+@pytest.mark.django_db
+def test_pk_model_to_df(pk_builtin_field_model_fixture, builtin_field_model_dataframe):
+    df_out = read_frame(
+        con=connection, qs=PkBuiltinFieldModel.objects.all(), drop_idx=False
+    )
+    assert_frame_equal(builtin_field_model_dataframe, df_out)
+
+
+@pytest.mark.django_db
+def test_view_to_df(builtin_view_dataframe, builtin_field_model_fixture):
+    assert "tests_builtinfieldmodel" in connection.introspection.table_names()
+
+    with connection.cursor() as c:
+        c.execute(
+            "CREATE VIEW view AS SELECT id, bigint, float, decimal FROM tests_builtinfieldmodel;"
+        )
+
+    df_out = read_frame(con=connection, qs=View.objects.all())
+
+    assert_frame_equal(builtin_view_dataframe, df_out)
